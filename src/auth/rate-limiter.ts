fa19deee7f4a34303718168ca8018b60aa3d0765
Akshay Pai <akshaypaik@gmail.com>
2025-04-30 10:22:52 +0530

    rate limter added to an api

---
+++ src/auth/rate-limiter.ts
@@ -0,0 +1,51 @@
+import Logger from "../logs/Logger";
+
+const MAX_REQUEST_COUNT = 30;
+
+class RateLimiter{
+    
+    public userInfo: any = {};
+
+    public setUserInfo(userDetails: any){
+        const userInfo = {
+            email: userDetails.email,
+            startTime: Date.now(),
+            // endTime: Date.now() + 60 * 1000,    // 1 minute
+            endTime: Date.now() + 60 * 1000,    // 60 minute
+            count: 1
+        }
+        this.userInfo[userDetails.email] = userInfo;
+    }
+    
+    public async checkRateLimiter(userDetails: any, res: any){
+        console.log(`check rate limiter started for user: ${userDetails.email}`);
+        if(!this.userInfo[userDetails.email]){
+            console.log(`setting user info object rate limiter for user: ${userDetails.email}`);
+            Logger.info(`setting user info object rate limiter for user: ${userDetails.email}`);
+            this.setUserInfo(userDetails);
+            return true;
+        }
+        else if(this.userInfo[userDetails.email].count < MAX_REQUEST_COUNT 
+            && Date.now() > this.userInfo[userDetails.email].startTime 
+            && Date.now() < this.userInfo[userDetails.email].endTime){
+            // allow api access
+            this.userInfo[userDetails.email].count += 1; 
+            console.log(`check rate limiter count: ${this.userInfo[userDetails.email].count}`);
+            Logger.info(`check rate limiter count: ${this.userInfo[userDetails.email].count}`);
+            return true;
+        } 
+        else if(Date.now() > this.userInfo[userDetails.email].endTime){
+            this.setUserInfo(userDetails);
+            return true;
+        }
+        else{
+            console.log(`check rate limiter ended with a deny for user: ${userDetails.email}`);
+            Logger.error(`check rate limiter ended with a deny for user: ${userDetails.email}`);
+            return false;
+           
+        }
+    }
+}
+
+const rateLimiter = new RateLimiter();
+export { rateLimiter };
--- src/controllers/interview-controller/InterviewController.ts
+++ src/controllers/interview-controller/InterviewController.ts
@@ -1,14 +1,26 @@
 import { Response, Request } from "express";
-import HttpStatus from "http-status-codes";
 import { interviewService } from "../../services/interview-service/InterviewService";
 import User from "src/models/DBCollectionSchemaModel/User.model";
+import { rateLimiter } from "../../auth/rate-limiter";
 
 class InterviewController {
     public async getInterviewCompaniesSearchResult(req: Request, res: Response) {
         try {
+
+            // check rate limit
+            const userDetails = {
+                email: req?.query?.email ? req?.query?.email?.toString() : "default"
+            }
+            const isValidReq = await rateLimiter.checkRateLimiter(userDetails, res);
+            if (!isValidReq) {
+                res.status(429).send({
+                    message: "Too many request. please try again after some time!"
+                });
+            }
+
             const searchQuery = req?.query?.searchQuery ? req?.query?.searchQuery?.toString() : "";
             const email = req?.query?.email ? req?.query?.email?.toString() : "";
-            if(email === "" || email === "undefined"){
+            if (email === "" || email === "undefined") {
                 res.status(500).send("Email is not valid");
             }
             const page = req?.query?.page ? Number(req?.query?.page) : 1;
@@ -25,8 +37,8 @@ class InterviewController {
         try {
             const searchQuery = req?.query?.searchQuery ? req?.query?.searchQuery?.toString() : "";
             const email = req?.query?.email ? req?.query?.email?.toString() : "";
-            const quickFilter =  req?.query?.quickFilter ? req?.query?.quickFilter?.toString() : "";
-            if(email === "" || email === "undefined"){
+            const quickFilter = req?.query?.quickFilter ? req?.query?.quickFilter?.toString() : "";
+            if (email === "" || email === "undefined") {
                 res.status(500).send("Email is not valid");
             }
             const page = req?.query?.page ? Number(req?.query?.page) : 1;
